0bb25ee9083ab48a1dad135c054b052b3ee724d6
Peter Nicholls <peterwnicholls@gmail.com>
2011-08-09 11:56:29 +1000

    more debugging

--- javascripts/core.js
+++ javascripts/core.js
@@ -612,16 +612,11 @@ Ext.override(Ext.Container, {
 Netzke.FeedbackGhost = function(){};
 Ext.apply(Netzke.FeedbackGhost.prototype, {
   showFeedback: function(msg){
-	console.log("-------------------");
-	console.log(msg);
     var createBox = function(s, l){
-	console.log(s);
-	console.log(l);
-        return ['<div class="drop_flash">',
-				'<h3>Wops, there is an error.</h3>',
-                '<p>',
-				s,
-				'</p>',
+        return ['<div class="msg">',
+                '<div class="x-box-tl"><div class="x-box-tr"><div class="x-box-tc"></div></div></div>',
+                '<div class="x-box-ml"><div class="x-box-mr"><div class="x-box-mc">', s, l, '</div></div></div>',
+                '<div class="x-box-bl"><div class="x-box-br"><div class="x-box-bc"></div></div></div>',
                 '</div>'].join('');
     }
 
