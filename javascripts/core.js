4ca0d453c59e2c77604593d22761d17943e029cf
Peter Nicholls <peterwnicholls@gmail.com>
2011-08-09 13:16:30 +1000

    debugging

--- javascripts/core.js
+++ javascripts/core.js
@@ -612,7 +612,7 @@ Ext.override(Ext.Container, {
 Netzke.FeedbackGhost = function(){};
 Ext.apply(Netzke.FeedbackGhost.prototype, {
   showFeedback: function(msg){
-	console.log(msg[1]);
+	console.log(msg[1].level);
     var createBox = function(s, l){
 	console.log(l);
 	
